392eeb6fd80fb83d20318013f89bc06dcfa844cc
Ksusha <hap-world@ya.ru>
2024-01-11 17:22:37 +0400

    AddLoops

--- script.js
+++ script.js
@@ -8,15 +8,35 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("What last movie did you see?", ""),
-    b = prompt("How would you rate it?", ""),
-    c = prompt("What last movie did you see?", ""),
-    d = prompt("How would you rate it?", "");
+for (let i = 0; i < 2; i++) {
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+    const a = prompt("What last movie did you see?", ""),
+          b = +prompt("How would you rate it?", "");
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }   
+}   
+    
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log('You have seen a few movies.');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are a classic viewer.');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are a movie fan.');
+} else {
+    console.log('Error');
+}
+
+
+
+
 
 
 
